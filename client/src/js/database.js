32c0538fb7f53dd710c8bee21d50a4d29cdaf9d5
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-16 09:38:10 -0700

    adding new transaction

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,6 +15,7 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.log('Update DB');
 const jateDB = await openDB('jate', 1);
+const tx = jateDB.transaction("jate", "readwrite");
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
